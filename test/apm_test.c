ee8c49131ae6f5c1cd14e5306914c685b5a4b7a9
Paul Wolneykien <manowar@altlinux.ru>
2010-03-17 16:00:08 +0300

    Add source of the test tool

---
+++ test/apm_test.c
@@ -0,0 +1,83 @@
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#define APM_IOC_PLLM_91 4
+#define APM_IOC_PLLM_179 8
+#define APM_IOC_USB_OFF 16
+#define APM_IOC_USB_ON 32
+#define APM_IOC_DDRSR_ON 64
+#define APM_IOC_DDRSR_OFF 128
+#define APM_IOC_PLLM_BYPASS 256
+#define APM_IOC_PLLM_PLL 512
+
+int main (int narg, char **argv, char **arge)
+{
+	int fd,ret,ctl;
+	char arg;
+
+	if (narg < 2)
+	{
+		arg = '-';
+	} else {
+		arg = argv[1][0];
+	}
+
+	switch (arg)
+	{
+		case 'b':
+			ctl = APM_IOC_PLLM_BYPASS;
+			break;
+		case 'B':
+			ctl = APM_IOC_PLLM_PLL;
+			break;
+		case 'c':
+			ctl = APM_IOC_PLLM_91;
+			break;
+		case 'C':
+			ctl = APM_IOC_PLLM_179;
+			break;
+		case 'u':
+			ctl = APM_IOC_USB_OFF;
+			break;
+		case 'U':
+			ctl = APM_IOC_USB_ON;
+			break;
+		case 'd':
+			ctl = APM_IOC_DDRSR_ON;
+			break;
+		case 'D':
+			ctl = APM_IOC_DDRSR_OFF;
+			break;
+		default:
+			printf("Usage: %s <arg>. Possible args:\n\
+\tb - PLL bypass mode on\n\
+\tB - PLL bypass mode off\n\
+\tc - minimal PLLM1 multiplier\n\
+\tC - maximal PLLM1 multiplier\n\
+\tu - power down USB PHY\n\
+\tU - power up USB PHY\n\
+\td - turn on DDR self-refresh (w/o DDR PHY power down)\n\
+\tD - turn off DDR self-refresh\n\
+\n",argv[0]);
+			return 255;
+	}
+
+	fd = open("/dev/misc/apm_bios",O_NONBLOCK | O_RDWR);
+	if (0 >= fd)
+	{
+		printf("can not open /dev/apm\n");
+		return fd;
+	}
+
+	ret = ioctl(fd,ctl);
+	if (0 != ret)
+	{
+		printf("error callin' ioctl\n");
+		return ret;
+	}
+
+	return 0;
+}
