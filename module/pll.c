d50257644263df5e6f755c95db92c00b8962f836
Paul Wolneykien <manowar@altlinux.ru>
2010-03-18 23:28:45 +0300

    Add debug output to the PLL transaction precedure

--- module/pll.c
+++ module/pll.c
@@ -29,26 +29,37 @@ static int pll_transaction(unsigned long base,
    * (input clock) to select the type of reference clock. Write
    * CLKMODE = 0 for internal oscillator. Write CLKMODE = 1 for square
    * wave input clock. */
+  DBG("Set CLKMODE to 0x%lx\n", refclk);
   write_reg_part(base, PLLCTL, PLLCTL_CLKMODE, refclk);
   /* In PLLCTL, write PLLENSRC = 0 (enable PLL enable). The bit PLLEN
    * in PLLCTL has no effect unless you write PLLENSRC = 0. */
+  DBG("Set PLLENSRC to 0 (enable PLL enable)\n");
   write_reg_part(base, PLLCTL, PLLCTL_PLLENSRC, 0);
   /* In PLLCTL, write PLLEN = 0 (bypass mode). */
+  DBG("Set PLLEN to 0 (bypass mode)\n");
   write_reg_part(base, PLLCTL, PLLCTL_PLLEN, 0);
   /* Wait at least 4 reference clock cycles for the PLLEN mux
    * to change.*/
-  do { udelay(400); } while (!test_reg(base, PLLCTL, PLLCTL_PLLEN));
+  do {
+    DBG("Wait for the register to change\n");
+    udelay(400);
+  } while (!test_reg(base, PLLCTL, PLLCTL_PLLEN));
   /* In PLLCTL, write PLLRST = 1 (assert PLL reset). */
+  DBG("Set PLLRST to 1 (assert PLL reset)\n");
   write_reg_part(base, PLLCTL, PLLCTL_PLLRST, 1);
   /* In PLLCTL, write PLLDIS = 1 (assert PLL disable). */
+  DBG("Set PLLDIS to 1 (assert PLL disable)\n");
   write_reg_part(base, PLLCTL, PLLCTL_PLLDIS, 1);
   /* In PLLCTL, write PLLPWRDN = 0 (power up the PLL). */
+  DBG("Set PLLPWRDN to 0 (power up the PLL)\n");
   write_reg_part(base, PLLCTL, PLLCTL_PWRDN, 0);
   /* In PLLCTL, write PLLDIS = 0 (de-assert PLL disable). */
+  DBG("Set PLLDIS to 0 (de-assert PLL disable)\n");
   write_reg_part(base, PLLCTL, PLLCTL_PLLDIS, 0);
   /* If necessary, write PREDIV, POSTDIV, and PLLM to set divider
    * and multiplier values. */
   if (mul > 0) {
+    DBG("Set PLLM to 0x%x\n", mul);
     write_reg_part(base, PLLM, PLLM_PLLM, mul);
   }
   /*  If necessary, write PLLDIV to set PLLDIVn dividers. Note that
@@ -56,13 +67,17 @@ static int pll_transaction(unsigned long base,
    * ratios. */
   // TODO PLLDIVn, etc.
   /* Wait at least 5 miro-seconds for the PLL reset. */
+  DBG("Wait for 5 us for PLL to reset\n");
   udelay(5);
   /* In PLLCTL, write PLLRST = 0 (de-assert PLL reset). */
+  DBG("Set PLLRST to 0 (de-assert PLL reset)\n");
   write_reg_part(base, PLLCTL, PLLCTL_PLLRST, 0);
   /* Wait at least 8000 reference clock cycles for the PLL to lock. */
+  DBG("Wait...\n");
   mdelay(100); // TODO wait for what?
   /* In PLLCTL, write PLLEN = 1 to (switch from bypass mode to PLL mode). */
   if (mul == 0) {
+    DBG("Set PLLEN to 1 (PLL mode)\n");
     write_reg_part(base, PLLCTL, PLLCTL_PLLEN, 1);
   }
 
