93a856e7d7cae7ba5074d5a43793619986d87233
Paul Wolneykien <manowar@altlinux.ru>
2010-03-23 00:56:08 +0300

    Format pll.h comments

--- module/pll.h
+++ module/pll.h
@@ -9,37 +9,37 @@
 
 /* Offset definitions for the PLL registers */
 
-/*Contains peripheral ID and revision information */
+/* Contains peripheral ID and revision information */
 #define PID 0x00
-/*Controls PLL operations */
+/* Controls PLL operations */
 #define PLLCTL 0x100
-/*PLL Multiplier Control */
+/* PLL Multiplier Control */
 #define PLLM 0x110
-/*Pre-divider control */
+/* Pre-divider control */
 #define PREDIV 0x114
-/*Divider 1 control-divider for SYSCLK1 */
+/* Divider 1 control-divider for SYSCLK1 */
 #define PLLDIV1 0x118
-/*Divider 2 control-divider for SYSCLK2 */
+/* Divider 2 control-divider for SYSCLK2 */
 #define PLLDIV2 0x11c
-/*Divider 3 control-divider for SYSCLK3 */
+/* Divider 3 control-divider for SYSCLK3 */
 #define PLLDIV3 0x120
-/*Post-divider control */
+/* Post-divider control */
 #define POSTDIV 0x128
-/*Bypass divider control */
+/* Bypass divider control */
 #define BPDIV 0x12c
-/*PLL controller command register */
+/* PLL controller command register */
 #define PLLCMD 0x138
-/*PLL controller status register */
+/* PLL controller status register */
 #define PLLSTAT 0x13c
-/*SYSCLKn divider ratio change and align control register */
+/* SYSCLKn divider ratio change and align control register */
 #define ALNCTL 0x140
-/*PLL divider ratio change status register */
+/* PLL divider ratio change status register */
 #define DCHANGE 0x144
-/*Clock enable control AUXCLK */
+/* Clock enable control AUXCLK */
 #define CKEN 0x148
-/*Clock status for SYSCLKBP and AUXCLK */
+/* Clock status for SYSCLKBP and AUXCLK */
 #define CKSTAT 0x14c
-/*Clock status for SYSCLKn clocks */
+/* Clock status for SYSCLKn clocks */
 #define SYSTAT 0x150
 /* Divider 4 control-divider for SYSCLK4 */
 #define PLLDIV4 0x160
