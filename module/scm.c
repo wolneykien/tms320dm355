7e3f5e0be69fe444306bea5b68dbdde4dc9a2be2
Paul Wolneykien <manowar@altlinux.ru>
2010-03-23 01:30:18 +0300

    Add new module for the System Control Module
    - USB_PHYPDWN support

---
+++ module/scm.c
@@ -0,0 +1,130 @@
+
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/miscdevice.h>
+#include <linux/device.h>
+#include <linux/kernel.h>
+
+#include <asm/system.h>
+#include <linux/delay.h>
+#include <mach/hardware.h>
+#include <mach/io.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
+#include <linux/stat.h>
+
+/* Include SCM memory map definitions */
+#include "scm.h"
+
+#define DRIVER_NAME "scm"
+
+/* Common procedures */
+#include "common.c"
+
+static const char driver_version[] = "1.0";
+
+/* USB PHY syfs objects */
+
+static ssize_t usb_power_state_show(struct kobject *kobj,
+				    struct kobj_attribute *attr,
+				    char *buf)
+{
+  unsigned long state;
+
+  state = read_reg_part(SCM_BASE, USB_PHY_CTRL, USB_PHYPDWN);
+  DBG("USB_PHYPWDN: 0x%lx\n", state);
+  return sprintf(buf, "%d\n", (state == 0 ? 1 : 0));
+}
+
+static ssize_t usb_power_state_store(struct kobject *kobj,
+				     struct kobj_attribute *attr,
+				     const char *buf, size_t count)
+{
+  unsigned long state;
+
+  if (count > 0) {
+    if (count > 2) {
+      return -ENOENT;
+    }
+    if (strncmp(buf, "0", 1) == 0) {
+      state = 1;
+    } else {
+      state = 0;
+    }
+
+    DBG("USB_PHYPWDN: 0x%lx -> 0x%lx\n",
+	read_reg_part(SCM_BASE, USB_PHY_CTRL, USB_PHYPDWN),
+	state);
+    write_reg_part(SCM_BASE, USB_PHY_CTRL, USB_PHYPDWN, state);
+  }
+
+  return count;
+}
+
+struct kobject *usb_kobj;
+struct kobject *usb_power;
+static struct kobj_attribute usb_power_state =
+  __ATTR(state, 0644, usb_power_state_show, usb_power_state_store);
+static struct attribute * usb_attrs[] =
+  {
+    &usb_power_state.attr,
+    NULL
+  };
+static struct attribute_group usb_attr_group =
+  {
+    .attrs = usb_attrs,
+  };
+
+static struct kobject* register_kobj(struct kobject *parent,
+				     const char *name,
+				     struct attribute_group *attr_group)
+{
+  struct kobject *kobj;
+  
+  
+  DBG("Register the %s kernel object\n", name);
+  if ((kobj = kobject_create_and_add(name, parent))) {
+    DBG("Register %s attributes\n", name);
+    if (sysfs_create_group(kobj,  attr_group) != 0) {
+      ERROR("Unable to register %s attributes\n", name);
+    }
+  } else {
+    ERROR("Unable to create %s kernel object\n", name);
+  }
+
+  return kobj;
+}
+
+struct kobject *arm_kobj;
+
+static int __init scm_init(void)
+{
+  struct kobject *kobj = NULL;
+
+  DBG("Register the kernel objects\n");
+  if ((arm_kobj = kobject_create_and_add("arm", NULL))) {
+    if ((kobj = kobject_create_and_add("usb", arm_kobj))) {
+      kobj = register_kobj(kobj, "power", &usb_attr_group);
+    }
+  }
+
+  if (arm_kobj == NULL || kobj == NULL) {
+    return -ENOMEM;
+  } else {
+    return 0;
+  }
+}
+
+static void __exit scm_exit(void)
+{
+  DBG("Unregister the kernel objects\n");
+  kobject_put(arm_kobj);
+  arm_kobj = NULL;
+}
+
+module_init(scm_init);
+module_exit(scm_exit);
+
+MODULE_AUTHOR("Paul Wolneykien");
+MODULE_DESCRIPTION("System control module for ARM DaVinci");
+MODULE_LICENSE("GPL");
---
+++ module/scm.h
@@ -0,0 +1,66 @@
+
+/* The system control module base address */
+#define SCM_BASE 0x01c40000
+
+/* SCM resugter offsets */
+
+/* PINMUX0 - Pin Mux 0 (Video In) Pin Mux Register */
+#define PINMUX0 0x0
+/* PINMUX1 - Pin Mux 1 (Video Out) Pin Mux Register*/
+#define PINMUX1 0x4
+/* PINMUX2 - Pin Mux 2 (AEMIF) Pin Mux Register */
+#define PINMUX2 0x8
+/* PINMUX3 - Pin Mux 3 (GIO/Misc) Pin Mux Register */
+#define PINMUX3 0xC
+/* PINMUX4 - Pin Mux 4 (Misc) Pin Mux Register */
+#define PINMUX4 0x10
+/* Boot Configuration */
+#define BOOTCFG 0x14
+/* Multiplexing Control for Interrupts */
+#define ARM_INTMUX 0x18
+/* Multiplexing Control for EDMA Events */
+#define EDMA_EVTMUX 0x1C
+/* DDR Slew Rate */
+#define DDR_SLEW 0x20
+/* CLKOUT div/out Control */
+#define CLKOUT 0x24
+/* Device ID */
+#define DEVICE_ID 0x28
+/* Video DAC Configuration */
+#define VDAC_CONFIG 0x2C
+/* TIMER64_CTL - Timer64+ Input Control */
+#define TIMER64_CTL 0x30
+/* USB PHY Control */
+#define USB_PHY_CTRL 0x34
+/* Miscellaneous Control */
+#define MISC 0x38
+/* Master Priorities Reg0 */
+#define MSTPRI0 0x3C
+/* Master Priorities Reg1 */
+#define MSTPRI1 0x40
+/* VPSS Clock Mux Control */
+#define VPSS_CLK_CTRL 0x44
+/* DEEPSLEEP Configuration */
+#define DEEPSLEEP 0x48
+/* DEBOUNCE - Debounce for GIO0 Input */
+#define DEBOUNCE0 0x50
+/* DEBOUNCE - Debounce for GIO1 Input */
+#define DEBOUNCE1 0x54
+/* DEBOUNCE - Debounce for GIO2 Input */
+#define DEBOUNCE2 0x58
+/* DEBOUNCE - Debounce for GIO3 Input */
+#define DEBOUNCE3 0x5C
+/* DEBOUNCE - Debounce for GIO4 Input */
+#define DEBOUNCE4 0x60
+/* DEBOUNCE - Debounce for GIO5 Input */
+#define DEBOUNCE5 0x64
+/* DEBOUNCE - Debounce for GIO6 Input */
+#define DEBOUNCE6 0x68
+/* DEBOUNCE - Debounce for GIO7 Input */
+#define DEBOUNCE7 0x6C
+/* VTP IO Control Register */
+#define VTPIOCR 0x70
+
+
+/* USB-PHY bit-map */
+#define USB_PHYPDWN 0x01
