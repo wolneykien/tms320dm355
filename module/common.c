89adaebc0436630d3603b26bccdeedc01d79c46e
Paul Wolneykien <manowar@altlinux.ru>
2010-03-23 11:14:28 +0300

    Fix missing bit shift in partial register operations

--- module/common.c
+++ module/common.c
@@ -31,12 +31,38 @@ static int test_reg(unsigned long base,
   return read_reg(base, offs) & mask;
 }
 
+/* Shift a given value left to match a given mask */
+static unsigned long shvall(unsigned long mask,
+			    unsigned long val)
+{
+  if (mask) {
+    while (! (mask & 0x01)) {
+      mask = mask >> 1;
+      val = val << 1;
+    }
+  }
+  return val;
+}
+
+/* Shift a given value matching a given mask right to lower bits */
+static unsigned long shvalr(unsigned long mask,
+			    unsigned long val)
+{
+  if (mask) {
+    while (! (mask & 0x01)) {
+      mask = mask >> 1;
+      val = val >> 1;
+    }
+  }
+  return val;
+}
+
 /* Reads the partial value of a register (offset) */
 static unsigned long read_reg_part(unsigned long base,
 				   unsigned long offs,
 				   unsigned long mask)
 {
-  return read_reg(base, offs) & mask;
+  return shvalr(mask, read_reg(base, offs) & mask);
 }
 
 /* Sets the partial value of a given register (offset) */
@@ -45,7 +71,10 @@ static void write_reg_part(unsigned long base,
 			   unsigned long mask,
 			   unsigned long val)
 {
-  write_reg(base, offs, (read_reg(base, offs) & ~mask) | (val & mask));
+  write_reg(base,
+	    offs,
+	    (read_reg(base, offs) & ~mask) \
+	    | (shvall(mask, val) & mask));
 }
 
 /* Sets and returns the partial value of a given register (offset) */
